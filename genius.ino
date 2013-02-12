8d3f60c37d5fda18f671574a21ee9223735cc58a
Lucas Polo <lucaspolo@lucaspolo.com.br>
2013-02-12 10:52:32 -0200

    Refatoração com extração de novas funções.

--- genius.ino
+++ genius.ino
@@ -1,50 +1,50 @@
 
 #define DO 33
-#define RE 37
-#define MI 41
-#define FA 44
+#define RE 73
+#define MI 165
+#define FA 349
 
 int numeros[100];
-int posicao = 0;
+int posicao;
 
 void setup() {
   Serial.begin(9600);
-  numeros[0] = 0;
   configuraPinos();
-  
+  inicializaNumeros();
+}
+
+void inicializaNumeros() {
+  for(int i = 0; i < 100; i++) numeros[i] = 0;
+  posicao = 0;
 }
 
 void configuraPinos() {
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+  for(int i = 2; i <= 5; i++) pinMode(i, INPUT);
+  for(int i = 9; i <= 12; i++) pinMode(i, OUTPUT);
 }
 
 void loop() {
   while(true) {
-    buzz();
     principal(); 
   }  
 }
 
 void principal() {
+  insereNovoNumero();
+  validaSequencia();
+}
+
+void insereNovoNumero() {
   int pinoNovo = numeroRandomico();
   numeros[posicao] = pinoNovo;
   posicao++;
-  numeros[posicao] = 0;
-  validaSequencia();
+  numeros[posicao] = 0; 
 }
 
 void validaSequencia() {
   exibirSequencia();
   for(int i = 0; numeros[i] != 0; i++) {
     int botaoLido = lerBotao();
-    Serial.println(botaoLido);
     if(botaoLido == numeros[i]) {
       piscaLed(numeros[i]);
     } else {
@@ -55,18 +55,18 @@ void validaSequencia() {
 }
 
 void perdeu() {
-  buzz();
-  buzz();
-  buzz();
+  buzz(3);
   numeros[0] = 0;
   posicao = 0;
 }
 
-void buzz() {
-  tone(8, 262, 8/1000);
-  delay(100);
-  noTone(8);
-  delay(100);
+void buzz(int repeticoes) {
+  for(int i = 0; i < repeticoes; i++) {
+    tone(8, 262, 8/1000);
+    delay(100);
+    noTone(8);
+    delay(100);
+  }
 }
 
 int numeroRandomico() {
@@ -91,19 +91,18 @@ void piscaLed(int pino) {
 void tom(int pino) {
   switch(pino) {
     case 9:
-      tone(8, 262, 8/1000);
+      tone(8, DO, 8/1000);
       break;
     case 10:
-      tone(8, 262, 8/1000);
+      tone(8, RE, 8/1000);
       break;
     case 11:
-      tone(8, 262, 8/1000);
+      tone(8, MI, 8/1000);
       break;
     case 12:
-      tone(8, 262, 8/1000);
+      tone(8, FA, 8/1000);
       break;
     default:
-      //tone(8, 262, 8/1000);
       break;
   }
   delay(250);
@@ -119,16 +118,4 @@ int lerBotao() {
       }
     }  
   } 
-}
-
-void teste() {
-  Serial.println("Iniciando funcao de teste");
-  numeros[0] = 9;
-  numeros[1] = 10;
-  numeros[2] = 11;
-  numeros[3] = 12;
-  numeros[4] = 0;
-  exibirSequencia();
-  lerBotao();
-  Serial.println("Finalizando funcao de teste");
 }
